2fa451879af9502af37bbcfbb4def369e3a0aefa
adviuriaraujo <85589853+adviuriaraujo@users.noreply.github.com>
2022-02-19 14:42:54 -0300

    refactor: refatora pegaArquivo com async await

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro))
 }
 
-function pegaArquivo(path) {
-    const encoding = 'utf-8'
-    fs.promises
-    .readFile(path, encoding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch((erro) => trataErro(erro))
+async function pegaArquivo(path) {
+    const enconding = 'utf-8'
+    try{
+        const texto = await fs.promises.readFile(path, enconding)
+        console.log(chalk.green(texto))
+    } catch(erro){
+        trataErro(erro)
+    }    
 }
+
+//FUNÇÃO ASSÍNCRONA COM THEN E CATCH
+//function pegaArquivo(path) {
+//    const encoding = 'utf-8'
+//    fs.promises
+//    .readFile(path, encoding)
+//    .then((texto) => console.log(chalk.green(texto)))
+//    .catch((erro) => trataErro(erro))
+//}
 
 pegaArquivo('./arquivos/texto1.md')
