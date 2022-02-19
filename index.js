23b3963be453e8ddcb6c1f6b6fbd43b0c4771dec
adviuriaraujo <85589853+adviuriaraujo@users.noreply.github.com>
2022-02-19 13:39:47 -0300

    feat: adiciona uma tratativa de erro

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-console.log("Vamos começar!")
+import chalk from "chalk"
+import fs from "fs"
 
-const paragrafo = "Texto retornado por uma função"
-
-function texto(string) {
-    return string
+function trataErro(erro) {
+    throw new Error(chalk.red(erro))
 }
 
-console.log(texto(paragrafo))
+function pegaArquivo(path) {
+    const encoding = 'utf-8'
+    fs.readFile(path, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro)
+        }
+        console.log(chalk.green(texto))
+    })
+}
+
+pegaArquivo('./arquivos/texto1.md')
